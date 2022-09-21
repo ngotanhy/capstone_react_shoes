965c0900e423ba7790a68abe38ca890048ddd50e
Hy Ngo <ngotanhy123456@gamil.com>
2022-09-21 14:36:51 +0700

    init capstone-shoes

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -2,34 +2,18 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 export default function Header() {
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-      <NavLink className="navbar-brand" to="/">Navbar</NavLink>
-      <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation" />
-      <div className="collapse navbar-collapse" id="collapsibleNavId">
-        <ul className="navbar-nav me-auto mt-2 mt-lg-0">
-          <li className="nav-item">
-            <NavLink className="nav-link active" to="" aria-current="page">Home <span className="visually-hidden">(current)</span></NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/login">login</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/register">Register</NavLink>
-          </li>
-          <li className="nav-item dropdown">
-            <NavLink className="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</NavLink>
-            <div className="dropdown-menu" aria-labelledby="dropdownId">
-              <NavLink className="dropdown-item"  to="/" >Action 1</NavLink>
-              <NavLink className="dropdown-item"  to="/" >Action 2</NavLink>
-            </div>
-          </li>
-        </ul>
-        <form className="d-flex my-2 my-lg-0">
-          <input className="form-control me-sm-2" type="text" placeholder="Search" />
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
+    <nav className="flex sm:justify-start space-x-4 bg-slate-600 text-white p-3">
+      {[
+        ['Home', '/home'],
+        ['Login', '/login'],
+        ['Profile', '/profile'],
+        ['Cart', '/cart'],
+        ['Cart', '/cart'],
+        ['Detail', '/detailProduct/'],
 
+      ].map(([title, url]) => (
+        <NavLink to={url} className="rounded-lg px-3 test py-2 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</NavLink>
+      ))}
+    </nav>
   )
 }
